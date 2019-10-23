7b5abac92184b2216e6680a38a593724a05d3ef6
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-23 15:39:25 -0400

    Finish sessions controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionsController < ApplicationController
+    def create 
+        @user = User.with_attached_profile_photo.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+        if @user
+            login!(@user)
+            render :show
+        else
+            attempted_user = User.find_by(username: params[:user][:username])
+            if attempted_user
+                render json: ["wrong password", {attempted_username: params[:user][:username]}], status: 401
+            else
+                render json: ["username was not found"], status: 404
+            end
+        end
+    end
+
+    def destroy
+        if !current_user
+            render json: ['no user to logout'], status: 404
+        end
+        logout!
+    end
+
+end
