f80cf4ee461fca787aa73e24573193c5ffbe9cb7
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-23 15:37:53 -0400

    Finish users controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,34 @@
+class Api::UsersController < ApplicationController
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render :show
+    end
+
+    def index
+        @users = User.all
+        render :index
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(user_params)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password, :first_name, :last_name)
+    end
+end
