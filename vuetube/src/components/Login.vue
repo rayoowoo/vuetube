ccb63114f19bb55a6e471b4cf5e8b137331e71f6
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-25 02:35:51 -0400

    Add basic styling to login page

--- vuetube/src/components/Login.vue
+++ vuetube/src/components/Login.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="login">
-      <h1>Sign in</h1>
-      <h2>to continue to VueTube</h2>
 
-    <form @submit.prevent="signin">
-      <input type="hidden" name="authenticity_token" :value="csrf">
-        <input v-model="username" type="username" placeholder="Username">
-        <input v-model="password" type="password" placeholder="Password">
-        <button>Submit</button>
-    </form>
+    <div class="login-content">
+        <h1>Sign in</h1>
+        <h2>to continue to VueTube</h2>
 
-    <span>Create account</span>
+      <form @submit.prevent="signin">
+        <input type="hidden" name="authenticity_token" :value="csrf">
+          <input v-model="username" type="username" placeholder="Username">
+          <input v-model="password" type="password" placeholder="Password">
+          <button>Submit</button>
+      </form>
+
+      <span>Create account</span>
+
+    </div>
   </div>
 </template>
 
@@ -39,7 +43,56 @@ export default {
 <style scoped>
 .login {
   width: 80em;
-  height: 100em;
   margin: 0 auto;
 }
+.login-content {
+  margin: 10em auto;
+  border-radius: 3px;
+  padding: 6em 0;
+  width: 30em;
+  text-align: center;
+  border: 1px solid black;
+}
+
+.login h1 {
+  font-size: 2em;
+  font-weight: bold;
+  margin-bottom: 0.5em;
+}
+
+.login h2 {
+  font-size: 1.25em;
+  margin-bottom: 2em;
+}
+
+.login-content form {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin-bottom: 3em;
+}
+
+input {
+  border: 1px solid black;
+  padding: 0.5em;
+  width: 20em;
+  align-self: center;
+  margin-bottom: 0.25em;
+  box-sizing: border-box;
+}
+
+button {
+  margin-top: 2em;
+  border: 1px solid black;
+  align-self: center;
+  padding: 0.5em 0;
+  width: 20em;
+  box-sizing: border-box;
+}
+
+button:hover {
+  background-color: lightgray;
+  cursor: pointer;
+}
+
 </style>
