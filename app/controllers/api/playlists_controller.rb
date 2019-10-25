ab7b6c62c795067c9d18c4d470376341b83a9b6b
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-25 14:13:56 -0400

    Add playlist backend

---
+++ app/controllers/api/playlists_controller.rb
@@ -0,0 +1,45 @@
+class PlaylistsController < ApplicationController
+    def index
+        @playlists = Playlist.all
+        render :index
+    end
+
+    def show
+        @playlist = Playlist.find(params[:id])
+        render :show
+    end
+
+    def create
+        @playlist = Playlist.new(playlist_params)
+        if @playlist.save
+            render :show
+        else
+            render json: @playlist.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @playlist = Playlist.find(params[:id])
+        if @playlist.update_attributes(playlist_params)
+            render :show
+        else
+            render json: @playlist.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @playlist = Playlist.find(params[:id])
+        if @playlist 
+            @playlist.destroy
+            render :show
+        else
+            render json: ['Playlist was not found'], status: 404
+        end
+    end
+
+    private
+
+    def playlist_params
+        params.require(:playlist).permit(:name, :user_id)
+    end
+end
