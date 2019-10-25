5201453315f8b7f6a740ddd622e510156109aefe
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-25 02:22:48 -0400

    Add basic styling to video page

--- vuetube/src/components/VideoPage.vue
+++ vuetube/src/components/VideoPage.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="video-page">
         <Navbar />
-        <iframe v-bind:src="video.url" frameborder="0"></iframe>
-        <h1>{{video.title}}</h1>
-        <p>{{video.uploader.firstName}} {{video.uploader.lastName}}</p>
+        <div class="video-page-content">
+            <iframe v-bind:src="video.url" frameborder="0" allowfullscreen></iframe>
+            <h1>{{video.title}}</h1>
+            <p>{{video.uploader.firstName}} {{video.uploader.lastName}}</p>
 
-        <p>{{video.description}}</p>
+            <p>{{video.description}}</p>
+        </div>
     </div>
 </template>
 
@@ -44,5 +46,23 @@ export default {
 </script>
 
 <style scoped>
+    .video-page-content {
+        width: 80%;
+        margin: 0 auto;
+    }
 
+    iframe {
+        width: 700px;
+        height: 400px;
+    }
+
+    h1 {
+        font-size: 2em;
+        font-weight: bold;
+        margin-top: 0.5em;
+    }
+
+    p {
+        margin-bottom: 1em;
+    }
 </style>
